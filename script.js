189ecb1c2a2bd77d0037a085ee4454cd0113bcbe
Anton <antonsoldatov1983gmail.comgit config --global user.name antonsoldatov1983@gmail.com>
2022-12-22 19:50:09 +0000

    MeetWithJS

--- script.js
+++ script.js
@@ -1,2 +1,18 @@
 /* document.getElementById("demo").innerHTML ="Hello Java Script";
 onclick = document.getElementById(demobtn).innerHTML = "Hi"; */
+function changeImage(){
+    var image = document.getElementById("myImage");
+        if(image.src.match("bulbon")){
+            image.src ="img/bulboff.png";
+        } else{
+            image.src ="img/bulbon.png";
+        }
+}
+
+function Changes(){
+    let x = document.getElementById("changeStyles");
+    x.style.fontSize = "25px";
+    x.style.color = "brown";
+    x.innerHTML = "New Text Only";
+}
+document.write(5*6);
